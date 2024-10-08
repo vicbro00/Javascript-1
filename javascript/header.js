8ef44158c9eff19aef37d39d1873a238d4290ba5
vicbro <vicbro@hotmail.no>
2024-10-08 15:55:34 +0200

    Made the pages more interactive and cleaner

---
+++ javascript/header.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  const x = document.getElementById('offScreen');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function toggleCart() {
+  const x = document.getElementById('cartMenu');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
