21da9d9b14dd03e01c21f3e4d3b7332509740009
vicbro <vicbro@hotmail.no>
2024-12-11 15:30:16 +0100

    Fixing checkout page

--- basic-js.js
+++ basic-js.js
@@ -6,5 +6,10 @@ function loadScreen() {
 }
 
 function showPage() {
-  document.getElementById('loader').style.display = 'none';
+  const loaderElement = document.getElementById('loading'); // Use the correct ID
+  if (loaderElement) {
+    loaderElement.style.display = 'none';
+  } else {
+    console.warn('Loading element not found.');
+  }
 }
